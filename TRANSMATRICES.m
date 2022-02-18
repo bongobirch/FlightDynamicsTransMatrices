a49108946e197ab0636cf5c63aa97781ebf91bd9
bongobirch <84354402+bongobirch@users.noreply.github.com>
2022-02-17 19:07:43 -0600

    Add files via upload

--- TRANSMATRICES.m
+++ TRANSMATRICES.m
@@ -11,6 +11,11 @@ transMat = [cos(theta)*cos(psi),cos(theta)*sin(psi),-sin(theta);...
     cos(phi)*sin(theta)*sin(psi)-(sin(phi)*cos(psi)),...
     cos(phi)*cos(theta)];
 
-iMat = [ 1 0 0; 0 cos(phi) sin(phi);0 -sin(phi) cos(phi)];
-jMat = [cos(theta) 0 -sin(theta); 0 1 0;sin(theta) 0 cos(theta)];
-kMat = [cos(psi) sin(psi) 0;-sin(psi) cos(psi) 0; 0 0 1];
+iMat_ph = [ 1 0 0; 0 cos(phi) sin(phi);0 -sin(phi) cos(phi)];
+jMat_th = [cos(theta) 0 -sin(theta); 0 1 0;sin(theta) 0 cos(theta)];
+kMat_ps = [cos(psi) sin(psi) 0;-sin(psi) cos(psi) 0; 0 0 1];
+
+% For a 3 2 1, you do I * J * K
+% Always do it backwards!!
+
+%% 3 1 3
