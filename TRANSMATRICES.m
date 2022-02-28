450accdd30b7d7bbf0c3fd861e0a37fb48f26973
bongobirch <84354402+bongobirch@users.noreply.github.com>
2022-02-27 21:16:03 -0600

    Shortened i j k
    and added translational comment

--- TRANSMATRICES.m
+++ TRANSMATRICES.m
@@ -19,25 +19,26 @@ transMat = [cos(theta)*cos(psi),cos(theta)*sin(psi),-sin(theta);...
 
 %% Full mats
 
-iMat_ph = [ 1 0 0; 0 cos(phi) sin(phi);0 -sin(phi) cos(phi)];
-jMat_th = [cos(theta) 0 -sin(theta); 0 1 0;sin(theta) 0 cos(theta)];
-kMat_ps = [cos(psi) sin(psi) 0;-sin(psi) cos(psi) 0; 0 0 1];
+i_ph = [ 1 0 0; 0 cos(phi) sin(phi);0 -sin(phi) cos(phi)];
+j_th = [cos(theta) 0 -sin(theta); 0 1 0;sin(theta) 0 cos(theta)];
+k_ps = [cos(psi) sin(psi) 0;-sin(psi) cos(psi) 0; 0 0 1];
 
 % For a 3 2 1, you do I * J * K
 % Always do phi theta psi (i j k)
 % Always do it backwards!!
 
-iMat_th = [ 1 0 0; 0 cos(theta) sin(theta);0 -sin(theta) cos(theta)];
-jMat_ps = [cos(psi) 0 -sin(psi); 0 1 0;sin(psi) 0 cos(psi)];
-kMat_ph = [cos(phi) sin(phi) 0;-sin(phi) cos(phi) 0; 0 0 1];
+i_th = [ 1 0 0; 0 cos(theta) sin(theta);0 -sin(theta) cos(theta)];
+j_ps = [cos(psi) 0 -sin(psi); 0 1 0;sin(psi) 0 cos(psi)];
+k_ph = [cos(phi) sin(phi) 0;-sin(phi) cos(phi) 0; 0 0 1];
 
-iMat_ps = [ 1 0 0; 0 cos(psi) sin(psi);0 -sin(psi) cos(psi)];
-jMat_ph = [cos(phi) 0 -sin(phi); 0 1 0;sin(phi) 0 cos(phi)];
-kMat_th = [cos(theta) sin(theta) 0;-sin(theta) cos(theta) 0; 0 0 1];
+i_ps = [ 1 0 0; 0 cos(psi) sin(psi);0 -sin(psi) cos(psi)];
+j_ph = [cos(phi) 0 -sin(phi); 0 1 0;sin(phi) 0 cos(phi)];
+k_th = [cos(theta) sin(theta) 0;-sin(theta) cos(theta) 0; 0 0 1];
 
 %% 313 example
 
-three13 = kMat_ph * iMat_th * kMat_ps;
+three13 = k_ph * i_th * k_ps;
 three13 = simplify(three13);
 
-Three13_trans = inv(three13) % This is [xd1 yd1 zd1]!
+Three13_trans = inv(three13) % This is [xd1 yd1 zd1]! 
+% (Translational is inverse.)
